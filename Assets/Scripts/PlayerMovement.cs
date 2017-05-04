9e87358dc74d709b7e3e30838bbbf0c272fbb77c
rfukutome <rfukutome@gmail.com>
2017-05-04 01:00:37 -0700

    Basic player movement completed Completed basic player movement and sprite rotations to accompany them. Still need to make advanced controls. Need to change infastructure for AI bots.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,9 @@ public class PlayerMovement : MonoBehaviour {
         NONE = 4
     };
 
-    private float max_player_speed = 6;
+
+    public GameObject player_orientation;
+    private float max_player_speed = 10;
     private float player_speed;
     private bool after_first_input;
     private Direction direction;
@@ -32,13 +34,43 @@ public class PlayerMovement : MonoBehaviour {
         Vector2 input_direction = new Vector2(input_x, input_y);
 
         //Get which direction:
-        float input_angle = Mathf.Atan(input_y / input_x);
+        float input_angle = (Mathf.Atan(input_y / input_x))*(180/Mathf.PI);
+
+        if(input_y < 0 && input_x >= 0)
+        {
+            input_angle += 360;
+        }
+        else if(input_x < 0 && input_y >= 0 || input_x < 0 && input_y < 0)
+        {
+            input_angle += 180;
+        }
 
         //If you're not moving, take any direction
         if (!after_first_input && input_direction != Vector2.zero)
         {
             PickRandomDirection();
             after_first_input = true;
+        }
+
+        if ((input_angle >= 315 || input_angle < 45) && direction != Direction.LEFT)
+        {
+            direction = Direction.RIGHT;
+            player_orientation.transform.eulerAngles = new Vector3(0, 0, 270);
+        }
+        else if (input_angle >= 45 && input_angle < 135 && direction != Direction.DOWN)
+        {
+            direction = Direction.UP;
+            player_orientation.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (input_angle >= 135 && input_angle < 225 && direction != Direction.RIGHT)
+        {
+            direction = Direction.LEFT;
+            player_orientation.transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (input_angle >= 225 && input_angle < 315 && direction != Direction.UP)
+        {
+            direction = Direction.DOWN;
+            player_orientation.transform.eulerAngles = new Vector3(0, 0, 180);
         }
 
         switch (direction)
@@ -79,7 +111,5 @@ public class PlayerMovement : MonoBehaviour {
                 direction = Direction.LEFT;
                 break;
         }
-        Debug.Log(direction);
-
     }
 }
