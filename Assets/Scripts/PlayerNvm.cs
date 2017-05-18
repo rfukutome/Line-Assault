846e7fe29b0e4adba9350052a3650ffd7bda7dd8
Richard Fukutome <rfukutome@gmail.com>
2017-05-17 21:21:05 -0700

    Adding a new script to keep track of player data that should not be deleted upon death.

---
+++ Assets/Scripts/PlayerNvm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class will be used to keep all of the persistent player parameters.
+public class PlayerNvm : MonoBehaviour {
+
+    private string player_name;
+    private Color player_name_color;
+    private int high_score;
+    
+    public string GetPlayerName()
+    {
+        return player_name;
+    }
+
+    public Color GetPlayerNameColor()
+    {
+        return player_name_color;
+    }
+}
