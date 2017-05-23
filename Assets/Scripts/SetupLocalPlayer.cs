0166112d27972f0319e0611384768959cc385da9
Richard Fukutome <rfukutome@gmail.com>
2017-05-23 00:07:41 -0700

    Adding a script to correctly set up a local player vs a networked player on the player client

---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<PlayerMovement>().enabled = true;
+            transform.gameObject.tag = "OwnPlayer";
+        }
+        else
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            transform.gameObject.tag = "OtherPlayer";
+        }
+	}
+}
